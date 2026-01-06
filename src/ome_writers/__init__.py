c9d92ba9394da216e398e88c57f45c263827f262
fdrgsp <federico.gasparoli@gmail.com>
2026-01-06 10:28:39 -0500

    refactor: consolidate TensorStoreZarrStream import and remove redundant test file

--- src/ome_writers/__init__.py
+++ src/ome_writers/__init__.py
@@ -14,9 +14,8 @@ from ._dimensions import Dimension, DimensionLabel, UnitTuple
 from ._stream_base import OMEStream
 from ._util import dims_from_useq, fake_data_for_sizes
 from .backends._acquire_zarr import AcquireZarrStream
-from .backends._tensorstore import TensorStoreZarrStream
 from .backends._tifffile import TifffileStream
-from .backends._yaozarrs import ZarrPythonStream
+from .backends._yaozarrs import TensorStoreZarrStream, ZarrPythonStream
 
 __all__ = [
     "AcquireZarrStream",
--- src/ome_writers/backends/_tensorstore.py
+++
@@ -1,7 +0,0 @@
-"""TensorStore backend - re-exported from yaozarrs for consistency."""
-
-from __future__ import annotations
-
-from ._yaozarrs import TensorStoreZarrStream
-
-__all__ = ["TensorStoreZarrStream"]
