1f79a0299d9a58b0939b3c5310899f55ab5104ee
Talley Lambert <talley.lambert@gmail.com>
2026-01-14 09:16:02 -0500

    make example work again

--- examples/new_schema_image.py
+++ examples/new_schema_image.py
@@ -12,31 +12,34 @@ from ome_writers.schema_pydantic import (
     PositionDimension,
 )
 
-# or... in
+dimensions = [
+    Dimension(name="t", count=10, chunk_size=1, type="time"),
+    PositionDimension(positions=["Pos0"]),  # type: ignore
+    Dimension(name="c", count=2, chunk_size=1, type="channel"),
+    Dimension(name="z", count=5, chunk_size=1, type="space"),
+    Dimension(name="y", count=512, chunk_size=64, type="space"),
+    Dimension(name="x", count=512, chunk_size=64, type="space"),
+]
+
+# or... use the helper function:
+# from ome_writers.schema_pydantic import dims_from_standard_axes
+# dimensions = dims_from_standard_axes(
+#     sizes={"t": 10, "p": ["Pos0"], "c": 2, "z": 5, "y": 512, "x": 512},
+#     chunk_shapes={"y": 64, "x": 64},
+# )
 
 settings = AcquisitionSettings(
     root_path="output.ome.zarr",
-    array_settings=ArraySettings(
-        dimensions=[
-            Dimension(name="t", count=10, chunk_size=1, type="time"),
-            PositionDimension(positions=["Pos0"]),  # type: ignore
-            Dimension(name="c", count=2, chunk_size=1, type="channel"),
-            Dimension(name="z", count=5, chunk_size=1, type="space"),
-            Dimension(name="y", count=512, chunk_size=64, type="space"),
-            Dimension(name="x", count=512, chunk_size=64, type="space"),
-        ],
-        dtype="uint16",
-    ),
+    array_settings=ArraySettings(dimensions=dimensions, dtype="uint16"),
     overwrite=True,
     backend="auto",
 )
 
-
+shape = cast("tuple[int, ...]", settings.array_settings.shape)
+dtype = settings.array_settings.dtype
 with create_stream(settings) as stream:
-    shape = cast("tuple[int, ...]", settings.array_settings.shape)
     for i in range(np.prod(shape[:-2])):
-        frame = np.full(shape[-2:], i, dtype=settings.array_settings.dtype)
-        stream.append(frame)
+        stream.append(np.full(shape[-2:], i, dtype=dtype))
 
 # Validate the output
 import yaozarrs
