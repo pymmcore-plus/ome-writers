dc7477dad0dd19d291d1fd032d2a41787e3d3791
federico gasparoli <70725613+fdrgsp@users.noreply.github.com>
2026-02-03 19:32:23 -0500

    Update examples/pymmcore_plus_example.py
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>

--- examples/pymmcore_plus_example.py
+++ examples/pymmcore_plus_example.py
@@ -49,7 +49,9 @@ pixel_size_um = core.getPixelSizeUm()
 settings = AcquisitionSettings(
     root_path="example_pymmcore_plus",
     # use useq_to_acquisition_settings to convert MDASequence to ome_writers
-    # AcquisitionSettings `dimensions` and `plate` fields
+    # use useq_to_acquisition_settings to convert MDASequence to a dictionary
+    # of kwargs that can be passed to `ome_writers.AcquisitionSettings`
+    # (currently, it will populate `dimensions` and `plate` fields)
     **useq_to_acquisition_settings(
         seq,
         image_width=image_width,
